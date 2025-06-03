a01f33219fc15e4159e402c87b3af9e1063946e1
Juan García <101433757+MaruchanMachine@users.noreply.github.com>
2025-06-03 15:49:09 -0700

    feat: add validate_course_field js

---
+++ js/create_course_validations/validate_course_fields.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let e = [
+      { name: "fullname", label: "Nombre completo del curso" },
+      { name: "shortname", label: "Nombre corto del curso" },
+    ],
+    a = document.querySelector("form");
+  a.addEventListener("submit", function (a) {
+    let t = !1;
+    e.forEach((e) => {
+      let a = document.querySelector(`input[name="${e.name}"]`);
+      if (!a) {
+        console.warn(`- Campo no encontrado: ${e.name}`);
+        return;
+      }
+      let l = `${e.name}-error`,
+        r = document.getElementById(l);
+      r ||
+        (((r = document.createElement("div")).id = l),
+        (r.style.color = "red"),
+        (r.style.fontSize = "0.9em"),
+        (r.style.marginTop = "4px"),
+        a.insertAdjacentElement("afterend", r)),
+        (r.innerHTML = "");
+      let o = a.value;
+      o.length > 100
+        ((r.innerHTML = `<br>- ${e.label} no puede tener m\xe1s de 100 caracteres.`),
+          (t = !0));
+    }),
+      t && a.preventDefault();
+  });
+});
