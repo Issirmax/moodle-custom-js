f03174f8bdba4b2465e2a3ca14cc2e104be5e376
isabelfloresl <isabel.flumbrera@gmail.com>
2025-07-18 14:49:05 -0700

    feat: add user notification script with improved logging on DOMContentLoaded

--- js/notifications/notifications_user_2.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",(function(){console.log("pruebaaa 3"),console.log("Script iniciado");const e=document.querySelector(".notification-area").querySelector(".content-area").querySelector(".content").querySelector("pre");console.log("Elementos encontrados:",e)}));
