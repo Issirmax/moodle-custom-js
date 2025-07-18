fd52e7fc3aa77690de0775e811aa114e0777da1c
isabelfloresl <isabel.flumbrera@gmail.com>
2025-07-18 14:02:01 -0700

    feat: enhanced HTML rendering

---
+++ js/notifications/user_notification.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Script iniciado");
+    
+    // Selectores más específicos para tu estructura
+    const preElements = document.querySelectorAll(
+        ".notification-area .content-area .content pre" 
+    );
+    
+    console.log("Elementos <pre> encontrados:", preElements.length);
+    console.log("Elementos encontrados:", preElements);
+
+    preElements.forEach((pre, index) => {
+
+        let text = pre.textContent || pre.innerText;
+
+        text = text.replace(/Hola, ([^,]+), /, 'Hola, $1,<br><br>');
+        text = text.replace(/ en el curso /, '<br><br>en el curso ');
+        text = text.replace(/ Puede revisar/, '<br><br>Puede revisar');
+
+        const urlRegex = /(https?:\/\/[^\s]+)/g;
+        text = text.replace(urlRegex, function(url) {
+            return `<a href="${url}" target="_blank">${url}</a>`;
+        });
+
+        text = text.replace(/\n/g, "<br>");
+
+        console.log("Texto procesado:", text);
+
+        const paragraph = document.createElement("p");
+        paragraph.innerHTML = text;
+
+        if (pre.parentNode) {
+            pre.parentNode.replaceChild(paragraph, pre);
+            console.log(`Elemento ${index + 1} reemplazado exitosamente`);
+        }
+    });
+});
